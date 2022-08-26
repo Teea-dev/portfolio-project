daa7e6d2552c259cf79b78ef522ee3769f2ba099
Ogunnoiki Adetokunbo <adetokunboogunnoiki28@gmail.com>
2022-08-26 01:20:54 +0100

    started working on the animation

--- toggle.js
+++ toggle.js
@@ -4,3 +4,16 @@ const navBar= document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click', ()=>{
     navBar.classList.toggle('active')
 })
+// gsap.utils.toArray('.sections').forEach(section => {
+    
+//     const bgColor= section.getAttribute("data");
+//     const color = gsap.timeline({paused:true}).to("#mainbg",{backgroundColor:bgColor,duration:1,
+//         overWrite:true})
+//         scrollTrigger.create({
+//             trigger:section,
+//             start:"top 50%",
+//             onEnter:()=> color.play(),
+//             onLeaveBack:()=>color.reverse(),
+//         });
+//     })
+gsap.from(".top",{duration:1, y:"-100%" , ease:'bounce'})
